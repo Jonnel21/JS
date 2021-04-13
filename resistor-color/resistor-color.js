b77264591c7371b3511f4d6588787602186e768d
Jonnel <jonnel_alcantara@bio-rad.com>
2021-04-13 14:45:28 -0700

    implement solution

--- resistor-color/resistor-color.js
+++ resistor-color/resistor-color.js
@@ -3,8 +3,21 @@
 // convenience to get you started writing code faster.
 //
 
-export const colorCode = () => {
-  throw new Error('Remove this statement and implement this function');
+export const colorCode = (color) => {
+  for(let i = 0; i < COLORS.length; i++) {
+    if(color == COLORS[i]) { return i }
+  }
 };
 
-export const COLORS = undefined;
+export const COLORS = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white',
+];
